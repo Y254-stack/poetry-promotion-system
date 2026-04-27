ac8b1112eb79c0b14a91504abe39ea15c60375e0
WJosieF <3564283130@qq.com>
2026-04-27 21:20:53 +0800

    镜像配置

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,18 @@
 pluginManagement {
     repositories {
+        // 1. 阿里云的 Gradle 插件镜像 (用于替换 gradlePluginPortal())
+        maven {
+            url = uri("https://maven.aliyun.com/repository/gradle-plugin")
+        }
+        // 2. 阿里云的 Google 插件镜像 (用于替换 google())
+        maven {
+            url = uri("https://maven.aliyun.com/repository/google")
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
         google {
             content {
                 includeGroupByRegex("com\\.android.*")
@@ -14,6 +27,15 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        // 1. 阿里云的 Google 依赖镜像
+        maven {
+            url = uri("https://maven.aliyun.com/repository/google")
+        }
+        // 2. 阿里云的公共依赖镜像
+        maven {
+            url = uri("https://maven.aliyun.com/repository/public")
+        }
+
         google()
         mavenCentral()
     }
